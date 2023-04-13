9c1d5e97ee5e0d6df78be13b4bd275c78af2d49f
Janek <j.p.walaszek@gmail.com>
2023-04-13 23:34:56 +0200

    add hear for player and computer

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let playerLives = 5;
+let computerLives = 5;
+
 let playerChoice = '';
 
 function setPlayerChoice(choice) {
@@ -41,27 +44,33 @@ function getRoundResult(playerChoice, computerChoice) {
     }
     if (computerChoice === 'ROCK' && playerChoice === "PAPER") {
         playerScore++;
+        computerLives--;
         return 'you win!';
 
     }
     if (computerChoice === 'ROCK' && playerChoice === "SCISSORS") {
         computerScore++;
+        playerLives--;
         return 'you lost!'
     }
     if (computerChoice === 'PAPER' && playerChoice === "SCISSORS") {
         playerScore++;
+        computerLives--;
         return 'you win!';
     }
     if (computerChoice === 'PAPER' && playerChoice === "ROCK") {
         computerScore++;
+        playerLives--;
         return 'you lose!'
     }
     if (computerChoice === 'SCISSORS' && playerChoice === "ROCK") {
         playerScore++;
+        computerLives--;
         return 'you win!';
     }
     if (computerChoice === 'SCISSORS' && playerChoice === "PAPER") {
         computerScore++;
+        playerLives--;
         return 'you lose!'
     }
     else {
@@ -70,13 +79,10 @@ function getRoundResult(playerChoice, computerChoice) {
 };
 
 
-
 function playRound() {
-    // for (let i = 1; playerScore !== 1 && computerScore !== 1; i++) {
-    // console.log(`Round ${i}`);
+
     let computerChoice = getComputerChoice();
     let thisRoundResult = getRoundResult(playerChoice, computerChoice);
-    // }s
     
     document.getElementById("playerScore").innerHTML = playerScore;
     document.getElementById("computerScore").innerHTML = computerScore;
@@ -84,16 +90,59 @@ function playRound() {
     document.getElementById("computerChoice").innerHTML = computerChoice;
     document.getElementById("roundResult").innerHTML = thisRoundResult;
 
+    //Visualisation of health of Player and Computer
+    let fullHeart = "<img class=\"heart\" src=\"./img/heart.png\">";
+    let emptyHeart = "<img class=\"heart\" src=\"./img/emptyheart.png\">";
 
+
+    if (playerLives === 4) {
+        document.getElementById("playerLives").innerHTML = (fullHeart + fullHeart + fullHeart + fullHeart + emptyHeart)
+        
+    };
+
+    if (playerLives === 3) {
+        document.getElementById("playerLives").innerHTML = (fullHeart + fullHeart + fullHeart + emptyHeart + emptyHeart)
+    };
+
+    if (playerLives === 2) {
+        document.getElementById("playerLives").innerHTML = (fullHeart + fullHeart + emptyHeart + emptyHeart + emptyHeart)
+    };
+
+    if (playerLives === 1) {
+        document.getElementById("playerLives").innerHTML = (fullHeart + emptyHeart + emptyHeart + emptyHeart + emptyHeart)
+    };
+
+    if (playerLives === 0) {
+        document.getElementById("playerLives").innerHTML = (emptyHeart + emptyHeart + emptyHeart + emptyHeart + emptyHeart)
+    };
+
+    if (computerLives === 4) {
+        document.getElementById("computerLives").innerHTML = (fullHeart + fullHeart + fullHeart + fullHeart + emptyHeart)
+        
+    };
+
+    if (computerLives === 3) {
+        document.getElementById("computerLives").innerHTML = (fullHeart + fullHeart + fullHeart + emptyHeart + emptyHeart)
+    };
+
+    if (computerLives === 2) {
+        document.getElementById("computerLives").innerHTML = (fullHeart + fullHeart + emptyHeart + emptyHeart + emptyHeart)
+    };
+
+    if (computerLives === 1) {
+        document.getElementById("computerLives").innerHTML = (fullHeart + emptyHeart + emptyHeart + emptyHeart + emptyHeart)
+    };
     
+    if (computerLives === 0) {
+        document.getElementById("computerLives").innerHTML = (emptyHeart + emptyHeart + emptyHeart + emptyHeart + emptyHeart)
+    };
 
     if (playerScore >= 5 || computerScore >= 5) {
         document.getElementById("gameResultWindow").style.display = "block";
-        document.getElementById("game-container").style.display = "none";
+        document.getElementById("button-container").style.display = "none";
         document.getElementById("gameResult").innerText = gameResult();
-       }
+       };
     
-
     const gameRestartButton = document.getElementById("gameRestartButton");
     gameRestartButton.addEventListener("click", () => {
         window.location.reload();
